0c6f5f7b806e83e12a287c82d24958aa58cd2763
Haritha Weerathunga <39055744+HarithaWeerathunga@users.noreply.github.com>
2020-10-06 19:56:00 +0530

    Add files via upload

---
+++ src/pages/GoogleDrive.js
@@ -0,0 +1,27 @@
+import React from "react";
+import NormalButton from "../components/Button";
+import * as AiIcons from "react-icons/ai";
+
+
+
+
+
+function GoogleDrive(){
+
+
+    return(
+        <div className='home'>
+
+
+            <h1>Google Drive   </h1> <AiIcons.AiOutlineArrowRight/>
+
+
+        <NormalButton/>
+
+        </div>
+
+
+    )
+}
+
+export default GoogleDrive;
