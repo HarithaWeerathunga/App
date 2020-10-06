13e163fe787c845ed0cec3150fbda714d9b7e635
Haritha Weerathunga <39055744+HarithaWeerathunga@users.noreply.github.com>
2020-10-06 19:55:37 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+
+
+function Home(){
+    return(
+        <div className='home'>
+
+
+
+            <span><h1> Welcome To Easy Upload</h1></span> <AiIcons.AiOutlineCloudUpload/>
+            <h1>Go To Google Drive Tab to Upload</h1>
+
+        </div>
+
+
+
+    )
+}
+
+export default Home;
