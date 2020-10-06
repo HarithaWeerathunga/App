2496ade47aa4ce695dc4b5dd463f395968c49ce1
Haritha Weerathunga <39055744+HarithaWeerathunga@users.noreply.github.com>
2020-10-06 21:19:24 +0530

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Button } from 'semantic-ui-react'
 
 
-
+//method to proceed to the backend
 const proceedToGoogleDrive = (e)=> {
     e.preventDefault();
     window.location.href = "http://localhost:5000";
 }
 
-
+//initialize the button
 const NormalButton = () => (
 
 
